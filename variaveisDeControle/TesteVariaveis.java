5b82f830561ead087b4376d8593725d8d193cca7
Anderson Campos <andersonbcamposbr@gmail.com>
2024-12-19 23:25:22 -0300

    tese de media

--- variaveisDeControle/TesteVariaveis.java
+++
@@ -1,5 +0,0 @@
-public class TesteVariaveis{
-public static void main(String[] args) {
-
-    }
-}
---
+++ variaveisDeControle/Variaveis.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Variaveis {
+    public static void main(String[] args) {
+        int num1, num2, num3, num4, media, soma;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Digite um numero: ");
+        num1 = scanner.nextInt();
+
+        System.out.print("Digite um numero: ");
+        num2 = scanner.nextInt();
+
+        System.out.print("Digite um numero: ");
+        num3 = scanner.nextInt();
+
+        System.out.print("Digite um numero: ");
+        num4 = scanner.nextInt();
+
+        soma = num1 + num2 + num3 + num4;
+        media = soma / 4;
+
+        System.out.println("O resultado da soma é " + soma + ", e a média é " + media);
+    }
+}
